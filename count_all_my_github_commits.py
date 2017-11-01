7dd5da51b34f8bd378875573f421330d93170c0d
Yong Huang <yong.e.huang@gmail.com>
2017-11-01 16:52:59 +0100

    Print commits count for each repo.

--- count_all_my_github_commits.py
+++ count_all_my_github_commits.py
@@ -2,6 +2,7 @@ import requests
 
 """
 This script counts the total GitHub commits an user has pushed to all the repositories belonging to her.
+Give it a second try if encountering problem in response.
 """
 
 # Configuration section.
@@ -20,13 +21,16 @@ commits = []
 for repo_meta in all_repos_metadata:
     repo_url = repo_meta['url']
     repo_stats_url = repo_url + '/stats/contributors'
-    print('requesting: ', repo_stats_url)
+
     repo_stats = requests.get(repo_stats_url, headers=auth_header).json()
-    countribution_by_target_user = list(
+    contribution_by_target_user = list(
         filter(lambda contribution: github_username == contribution['author']['login'], repo_stats))
 
     # now we only have single json representing target user's contribution in the list
-    total_commits_on_repo = countribution_by_target_user[0]['total']
+    total_commits_on_repo = contribution_by_target_user[0]['total']
+
+    print('Commits:', total_commits_on_repo, ', on repo: ', repo_stats_url)
+
     commits.append(total_commits_on_repo)
 
 print('Total commits: ', sum(commits))

4edb81f2b2ccd33d80014aa2d8994c0a71e0e035
Yong Huang <yong.e.huang@gmail.com>
2017-11-01 10:09:59 +0100

    Add comment.

--- count_all_my_github_commits.py
+++ count_all_my_github_commits.py
@@ -1,11 +1,17 @@
 import requests
 
-# See: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+"""
+This script counts the total GitHub commits an user has pushed to all the repositories belonging to her.
+"""
+
+# Configuration section.
+
+# 1. Generate the token from here: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 token = ''
+# 2. Specify the target user
+github_username = ''
 
 auth_header = {'Authorization': 'token ' + token}
-
-github_username = ''  # the target user
 repos_url = 'https://api.github.com/users/' + github_username + '/repos'
 all_repos_metadata = requests.get(repos_url, headers=auth_header).json()
 
